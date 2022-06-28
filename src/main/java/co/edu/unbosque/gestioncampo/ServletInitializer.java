d0a57b8512dd324addbe5e25be31461e44241258
RobinsonF <rgutierrezs@unbosque.edu.co>
2022-06-28 16:37:24 -0500

    Cambios

--- src/main/java/co/edu/unbosque/gestioncampo/ServletInitializer.java
+++ src/main/java/co/edu/unbosque/gestioncampo/ServletInitializer.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque;
 
+import co.edu.unbosque.gestioncampo.GestioncampoApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -7,7 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CursoApplication.class);
+		return application.sources(GestioncampoApplication.class);
 	}
 
 }
